b7f58072dc72f867124d236e89a9716bca24acc4
JuanCM <juancampuzano2356@gmail.com>
2024-07-30 17:00:14 -0500

    Finaliza la clase 5

--- project-1/routes/web.php
+++ project-1/routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * Route::get       | Consultar
+ * Route::post      | Guardar
+ * Route::delete    | Eliminar
+ * Route::put       | Actualizar
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('blog', function() {
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php' ],
+        ['id' => 2, 'title' => 'laravel', 'slug' => 'laravel' ],
+    ];
+    return view('blog', ['posts' => $posts]);
+});
+
+Route::get('blog/{slug}', function($slug) {
+    //consulta a base de datos
+    $post = $slug; 
+    return view('post', ['post' => $post]);
+});
+
+// Route::get('buscar', function(Request $request) {
+//     //consulta a base de datos
+//     return $request->all();
+// });
